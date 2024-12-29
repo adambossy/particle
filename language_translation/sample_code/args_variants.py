f8f4ab1b9ca73642ad80d4026b7612acee7ee678
Adam Bossy-Mendoza <adambossy@users.noreply.github.com>
2024-12-29 15:57:53 -0500

    Add sample code for edge cases

---
+++ language_translation/sample_code/args_variants.py
@@ -0,0 +1,6 @@
+def foo(bar, baz=None, *_args, **kw_args):
+    pass
+
+
+def qux(bar: int, baz: int = None, *_args, **kw_args):
+    pass
---
+++ language_translation/sample_code/retype_vars.py
@@ -0,0 +1,5 @@
+x = {"foo": "bar"}
+x = {"foo", "bar"}  # Rewrite type
+y = {"baz"}
+x.update(y)
+x = "foo bar"
